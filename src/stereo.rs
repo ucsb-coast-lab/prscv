add04050f4fb3d2ca44927ea67a748983ec08507
chrism <christophermoran@ucsb.edu>
2020-05-30 16:42:58 -0700

    Adds stereo pixel

--- src/stereo.rs
+++ src/stereo.rs
@@ -7,10 +7,25 @@ pub struct xyz {
     x: u32, y: u32, z: f32
 }
 
+#[repr(C)]
+#[derive(Debug)]
+pub struct StereoPixel {
+    pixel_x: u32, pixel_y: u32, r: u8, g: u8, b: u8, x: f32, y: f32, z: f32
+}
+
 use std::convert::TryInto;
 
 #[no_mangle]
 pub unsafe extern "C" fn parse_xyz_data(ptr: *const xyz, length: u32) {
+    let v = std::slice::from_raw_parts(ptr, length.try_into().unwrap());
+    println!("The length of the read-in slice is: {}", v.len());
+    for i in 0..v.len() {
+        println!("{:?}",v[i]);
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn parse_stereo_data(ptr: *const xyz, length: u32) {
     let v = std::slice::from_raw_parts(ptr, length.try_into().unwrap());
     println!("The length of the read-in slice is: {}", v.len());
     for i in 0..v.len() {
